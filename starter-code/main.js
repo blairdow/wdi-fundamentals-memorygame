cf950df4935bcfdf50f08defc351faea60201316
Blair Dowis <blairdow@gmail.com>
2016-09-23 07:23:12 -0700

    unit 10 work js to create card divs

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,14 +1,25 @@
-console.log("JS file is connected to HTML! Woo!")
+console.log("JS file is connected to HTML! Woo!");
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if(cardTwo === cardFour) {
+/* if(cardTwo === cardFour) {
     alert("You found a match!");
 }
 else if(cardTwo !== cardFour) {
     alert("Sorry, try again");
+}; */
+
+var gameBoard = document.getElementById('game-board');
+function createCards() {
+    for(var i=0; i<4; i++) {
+      var newCardDiv = document.createElement('div');
+      newCardDiv.className = 'card';
+      gameBoard.appendChild(newCardDiv);
+    };  
 };
+
+createCards();
 
