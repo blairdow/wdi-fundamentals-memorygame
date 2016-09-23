c1114bde7cf0fd1b9be112c7a39cbbddd5e39457
Blair Dowis <blairdow@gmail.com>
2016-09-23 08:05:26 -0700

    unit 11 work

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,7 @@
 console.log("JS file is connected to HTML! Woo!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
 /* if(cardTwo === cardFour) {
     alert("You found a match!");
@@ -14,12 +12,50 @@ else if(cardTwo !== cardFour) {
 
 var gameBoard = document.getElementById('game-board');
 function createCards() {
-    for(var i=0; i<4; i++) {
-      var newCardDiv = document.createElement('div');
-      newCardDiv.className = 'card';
-      gameBoard.appendChild(newCardDiv);
+    for(var i=0; i<cards.length; i++) {
+        var newCardDiv = document.createElement('div');
+        newCardDiv.className = 'card';
+        
+        newCardDiv.setAttribute('data-card', cards[i]);
+        newCardDiv.addEventListener('click', isTwoCards);
+        
+        gameBoard.appendChild(newCardDiv);
     };  
 };
 
 createCards();
+
+function isTwoCards() {
+    cardsInPlay.push(this.getAttribute('data-card'));
+    console.log(this.getAttribute('data-card'));
+    
+    if(this.getAttribute('data-card')==='king') {
+        this.innerHTML = "<img src='knight-1598216_640.png'>";
+    }
+    else {
+        this.innerHTML = "<img src='knight-1598250_640.png'>";
+    };
+    
+    if(cardsInPlay.length ===2) {
+        isMatch(cardsInPlay);
+        cardsInPlay = [];
+        this.innerHTML = "";
+    }
+    /* on alert - clear innerHTML */
+    function clearImg(){
+        cardsInPlay.innerHTML = "";
+    }
+    
+};
+
+
+/*why does it work with just [0] and [1]? -- because it is the two cardsInPlay */
+function isMatch(cards) {
+    if(cards[0] === cards[1]) {
+        alert("You found a match!");
+    } else {
+        alert("Sorry, try again.");
+    };
+    
+};
 
